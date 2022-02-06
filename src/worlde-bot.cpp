2aadbd8862dbc936f15b1f0ca66cfafabd8eef67
ChaseDurand <chase.durand@gmail.com>
2022-02-06 14:39:41 -0600

    Added wordlists

---
+++ src/worlde-bot.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "lists.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << solutions.size() << std::endl;
+    std::cout << dictionary.size() << std::endl;
+    return 0;
+}
