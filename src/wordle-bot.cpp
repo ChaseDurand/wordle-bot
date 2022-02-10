b8e61bd32b49112831b68cec997d2f5d8998f5ef
ChaseDurand <chase.durand@gmail.com>
2022-02-09 22:52:33 -0600

    Cleaned up code. Moved colors to new file.

--- src/wordle-bot.cpp
+++ src/wordle-bot.cpp
@@ -4,36 +4,23 @@
 #include <map>
 #include <thread>
 #include "lists.h"
+#include "ansi_colors.h"
 #define WORD_SIZE 5
 
-//the following are UBUNTU/LINUX, and MacOS ONLY terminal color codes.
-#define RESET   "\033[0m"
-#define BLACK   "\033[30m"      /* Black */
-#define RED     "\033[31m"      /* Red */
-#define GREEN   "\033[32m"      /* Green */
-#define YELLOW  "\033[33m"      /* Yellow */
-#define BLUE    "\033[34m"      /* Blue */
-#define MAGENTA "\033[35m"      /* Magenta */
-#define CYAN    "\033[36m"      /* Cyan */
-#define WHITE   "\033[37m"      /* White */
-#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
-#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
-#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
-#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
-#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
-#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
-#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
-#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
+// Standardize solution/answer language
 
-
-void getDist(std::string word, std::vector<std::vector<int>> &solutionDistribution){
+// Add a word to a letter distribution table
+void addToDist(std::string word,
+    std::vector<std::vector<int>> &solutionDistribution){
     for(int i = 0; i < WORD_SIZE; ++i){
         solutionDistribution[i][word[i]-97]++;
     }
     return;
 }
 
-int getScore(std::string word, std::vector<std::vector<int>> &solutionDistribution){
+// Get a word's score based on a letter distribution table
+int getDistScore(std::string word,
+    std::vector<std::vector<int>> &solutionDistribution){
     int sum = 0;
     for(int i = 0; i < WORD_SIZE; ++i){
         sum += solutionDistribution[i][word[i]-97];
@@ -41,16 +28,18 @@ int getScore(std::string word, std::vector<std::vector<int>> &solutionDistributi
     return sum;
 }
 
-void checkGuess(std::string guess, std::string answer, std::vector<char> &grn, std::map<char, std::vector<int>> &ylw, std::map<char, std::vector<int>> &gry){
-    std::vector<char> lettersNotLit = {};
-    std::vector<std::string> highlightMask = {WHITE, WHITE, WHITE, WHITE, WHITE};
-    for(int i = 0; i < WORD_SIZE; ++i){
-        lettersNotLit.push_back(answer[i]);
-    }
+// Compare a word against an answer and record greens/yellows/greys
+void checkGuess(std::string guess, std::string answer,
+    std::vector<char> &grn, std::map<char, std::vector<int>> &ylw,
+    std::map<char, std::vector<int>> &gry){
+    // Keep track of letters not colored for accurate letter counting.
+    std::vector<char> lettersRemaining(answer.begin(), answer.end());
+    // Use color codes to store color mask
+    std::vector<std::string> highlightMask(WORD_SIZE, RESET);
     // Identify greens
     for(int i = 0; i < WORD_SIZE; ++i){
         if(guess[i] == answer[i]){
-            lettersNotLit.erase(std::find(lettersNotLit.begin(), lettersNotLit.end(), guess[i]));
+            lettersRemaining.erase(std::find(lettersRemaining.begin(), lettersRemaining.end(), guess[i]));
             highlightMask[i] = GREEN;
             grn[i] = guess[i];
         }
@@ -58,16 +47,16 @@ void checkGuess(std::string guess, std::string answer, std::vector<char> &grn, s
     // Identify yellows
     for(int i = 0; i < WORD_SIZE; ++i){
         if(highlightMask[i] != GREEN){
-            if(std::find(lettersNotLit.begin(), lettersNotLit.end(), guess[i]) != lettersNotLit.end()){
-                lettersNotLit.erase(std::find(lettersNotLit.begin(), lettersNotLit.end(), guess[i]));
+            if(std::find(lettersRemaining.begin(), lettersRemaining.end(), guess[i]) != lettersRemaining.end()){
+                lettersRemaining.erase(std::find(lettersRemaining.begin(), lettersRemaining.end(), guess[i]));
                 highlightMask[i] = YELLOW;
                 ylw[guess[i]].push_back(i);
             }
         }
     }
-    // Identify greys
+    // Identify greys and print guess result
     for(int i = 0; i < WORD_SIZE; ++i){
-        if (highlightMask[i] == WHITE){
+        if (highlightMask[i] == RESET){
             gry[guess[i]].push_back(i);
         }
         std::cout << highlightMask[i] << guess[i] << RESET;
@@ -76,10 +65,13 @@ void checkGuess(std::string guess, std::string answer, std::vector<char> &grn, s
     return;
 }
 
-void reduceList(std::vector<std::string> &list, std::vector<char> &grn, std::map<char, std::vector<int>> &ylw, std::map<char, std::vector<int>> &gry){
+// Given a list and color results, remove impossible values
+void reduceList(std::vector<std::string> &list, std::vector<char> &grn,
+    std::map<char, std::vector<int>> &ylw, std::map<char,
+    std::vector<int>> &gry){
     int initialSize = list.size();
     int removed = 0;
-
+    // TODO separate this lambda
     list.erase(std::remove_if(list.begin(), list.end(), [grn, ylw, gry](std::string word){
         // Filter by greens
         for(int i = 0; i < WORD_SIZE; ++i){
@@ -90,12 +82,9 @@ void reduceList(std::vector<std::string> &list, std::vector<char> &grn, std::map
                 }
             }
         }
-
-
         // Filter by yellows
         // For every yellow character
         for(auto iter = ylw.begin(); iter != ylw.end(); ++iter){
-
             // For every yellow character's position
             for(int yellowPositions : iter->second){
                 if(iter->first == word[yellowPositions]){
@@ -125,9 +114,9 @@ void reduceList(std::vector<std::string> &list, std::vector<char> &grn, std::map
                 }
             }
         }
-
         // Filter by greys
         // For every grey character
+        // TODO check if this checks all positions, not just where grey was found
         for(auto iter = gry.begin(); iter != gry.end(); ++iter){
             // For every grey character's position
             for(int greyPositions : iter->second){
@@ -137,21 +126,23 @@ void reduceList(std::vector<std::string> &list, std::vector<char> &grn, std::map
                 }
             }
         }
-
         return false;
-
     }), list.end());
     return;
 }
 
+// Return true if we have all green letters
 bool isSolved(std::vector<char> greens){
     return std::count(greens.begin(), greens.end(),'_') == 0;
 }
 
+// Given an answer, answer list, and total list, play a game until winning.
+// Output the game result (answer,rounds) to file.
 void playGame(std::string answer, std::vector<std::string> answerSpace, std::vector<std::string> totalSpace){
     int round = 0;
     std::cout << "Starting game. Target word: " << answer << std::endl;
     std::vector<std::string> solutionSpace = solutions;
+    // TODO convert all colors to same struct
     // greens
     // list of letter and position
     // list of 5 letters
@@ -160,41 +151,43 @@ void playGame(std::string answer, std::vector<std::string> answerSpace, std::vec
     // list of letters and positions
     std::map<char, std::vector<int>> yellows;
     // greys
-    // list of letters
+    // list of letters and positions
     std::map<char, std::vector<int>> greys;
 
     // Calculate distribution of letters per position of answers
     std::vector<std::vector<int>> answerCharDist(WORD_SIZE, std::vector<int>(26, 0));
     for(std::string word : solutionSpace){
         // Add to distribution
-        getDist(word, answerCharDist);
+        addToDist(word, answerCharDist);
     }
 
-
+    // Record word and score, keeping track of max to determine best guess
     std::map<std::string, int> wordScore = {};
-    // Determine best guess
-    // For each word in total list, calculate "score" to find best first guess
     int maxSum = 0;
     int sum = 0;
     std::string maxWord = "";
     for(std::string word : totalSpace){
-        sum = getScore(word, answerCharDist);
+        sum = getDistScore(word, answerCharDist);
         wordScore.insert(std::pair<std::string, int>(word, sum));
         if (sum > maxSum){
             maxSum = sum;
             maxWord = word;
         }
     }
-
     
+    // Keep guessing until game is solved
     while(!isSolved(greens)){
-        std::cout << "Round " << ++round << ", "<< "guessing " << maxWord << std::endl;
+        // TODO move to playRound function
+        if(++round > 6){
+            std::cout << RED;
+        }
+        std::cout << "Round " << round << ": " << "guessing " << maxWord
+            << RESET << std::endl;
         checkGuess(maxWord, answer, greens, yellows, greys);
-
         if(isSolved(greens)){
             break;
         }
-
+        // Output greens and yellows for debugging
         std::cout << "Greens: ";
         for(int i = 0 ; i < WORD_SIZE; ++i){
             std::cout << greens[i];
@@ -209,15 +202,14 @@ void playGame(std::string answer, std::vector<std::string> answerSpace, std::vec
         }
         std::cout << std::endl;
 
-
         // Reduce solution space
         reduceList(solutionSpace, greens, yellows, greys);
 
         // Reduce dictionary space
         reduceList(totalSpace, greens, yellows, greys);
 
-
         // Remove guess from solution and dictionary spaces
+        // TODO replace with erase+remove_if
         if(std::find(solutionSpace.begin(), solutionSpace.end(), maxWord) != solutionSpace.end()){
             solutionSpace.erase(std::find(solutionSpace.begin(), solutionSpace.end(), maxWord));
         }
@@ -225,25 +217,25 @@ void playGame(std::string answer, std::vector<std::string> answerSpace, std::vec
             totalSpace.erase(std::find(totalSpace.begin(), totalSpace.end(), maxWord));
         } 
 
-
         // Reset yellows
+        // TODO check if carrying this over helps
+        // Possibly carry over and remove new greens to see what remains?
         yellows.clear();
 
-        // Calculate distribution of letters per position of answers
-        std::vector<std::vector<int>> answerCharDist(WORD_SIZE, std::vector<int>(26, 0));
+        // Calculate new distribution of letters per position of answers
+        std::vector<std::vector<int>> answerCharDist(WORD_SIZE,
+                                      std::vector<int>(26, 0)  );
         for(std::string word : solutionSpace){
-            // Add to distribution
-            getDist(word, answerCharDist);
+            addToDist(word, answerCharDist);
         }
 
-        // Determine best guess
-        // For each word in total list, calculate "score" to find best first guess
+        // Calculate score from new distribution to find best guess
         maxSum = 0;
         sum = 0;
         maxWord = "";
         for(std::string word : totalSpace){
             // std::cout << word << std::endl;
-            sum = getScore(word, answerCharDist);
+            sum = getDistScore(word, answerCharDist);
             wordScore.insert(std::pair<std::string, int>(word, sum));
             if (sum > maxSum){
                 maxSum = sum;
@@ -251,6 +243,8 @@ void playGame(std::string answer, std::vector<std::string> answerSpace, std::vec
             }
         }
     }
+
+    // Write results to file and terminal
     std::ofstream gameResults;
     gameResults.open("gameResults.txt", std::ios_base::app);
     std::cout << answer << ',' << round << '\n';
@@ -261,11 +255,14 @@ void playGame(std::string answer, std::vector<std::string> answerSpace, std::vec
 
 int main() {
     std::cout << "Running..." << std::endl;
+    
+    // Merge solution and dictionary to get total guess list
     std::vector<std::string> totalList = solutions;
     totalList.insert(totalList.end(), dictionary.begin(), dictionary.end());
-    std::map<std::string, int> wordScore = {};
-    std::vector<std::string> tmpSolutions(solutions.begin(), solutions.end());
-    for(std::string answer : tmpSolutions){
+    
+    // Modify to test on subset of answer list
+    std::vector<std::string> solutionSubset(solutions.begin(), solutions.end());
+    for(std::string answer : solutionSubset){
         playGame(answer, solutions, totalList);
     }
     return 0;
